1923d1fdb3ec2d416175c2504599a8c0962d0c8b
Phil <phil@sourcetoad.com>
2018-04-27 17:55:10 -0400

    fixed dead development evironment

--- gulpFile.js
+++ gulpFile.js
@@ -1,19 +1,22 @@
+'use-strict';
+
 var gulp = require('gulp'),
-	webserver = require('gulp-webserver'),
-	useref = require('gulp-useref');
+    webserver = require('gulp-webserver'),
+    useref = require('gulp-useref');
 
 gulp.task('webserver', function() {
   gulp.src('.')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: false,
-      open: true,
-	  fallback: 'index.html'
-    }));
+  .pipe(webserver({
+    livereload: true,
+    directoryListing: false,
+    open: true,
+    port: 8088,
+    fallback: 'index.html'
+  }));
 });
 
 gulp.task('build', function(){
-	return gulp.src('./*.html')
+  return gulp.src('./*.html')
         .pipe(useref())
         .pipe(gulp.dest('dist'));
 });
